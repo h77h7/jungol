326e1983c445146ef4073341de784d1e24c84908
h77h7 <74125523+h77h7@users.noreply.github.com>
2021-04-01 17:08:53 +0900

    수정2
    time : 2ms
    visit 배열 사용하여 공통 조상 체크

--- j1440.cpp
+++ j1440.cpp
@@ -20,7 +20,6 @@ int main() {
     int parent, child;
     for (int i = 0; i < N-1; i++) {
         cin >> parent >> child;
- 
         tree[child] = parent;
     }
  
@@ -28,19 +27,27 @@ int main() {
  
     cin >> v1 >> v2;
  
-    int orV2 = v2;
     int commonP = 0;
-    while (v1) {
-        while (v2) {
-            if (v1 == v2) {
-                commonP = v1;
-                break;
-            }
-            v2 = tree[v2];
+    bool visit1[TREE_SIZE] = { false, };
+    bool visit2[TREE_SIZE] = { false, };
+ 
+    for (int i = 0; i < TREE_SIZE; i++) {
+        if (v1 == v2) {
+            commonP = v1;
+            break;
         }
-        if (commonP) break;
-        v2 = orV2;
+        if (visit2[v1]) {
+            commonP = v1;
+            break;
+        }
+        else if (visit1[v2]) {
+            commonP = v2;
+            break;
+        }
+        visit1[v1] = true;
+        visit2[v2] = true;
         v1 = tree[v1];
+        v2 = tree[v2];
     }
  
     cout << commonP << endl;
