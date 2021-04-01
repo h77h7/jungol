13f533a47edc5f26401949379e5e4d92f0250bb1
h77h7 <74125523+h77h7@users.noreply.github.com>
2021-04-01 17:25:41 +0900

    Update j1141.cpp

--- j1141.cpp
+++ j1141.cpp
@@ -3,44 +3,30 @@
 using namespace std;
  
 int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+ 
     int N = 0;
-    int stack[80000] = { 0, };
-    int heights[80000] = { 0, };
-    int nums[80000] = { 0, };
-    int top = -1;
+    //int heights[80000] = { 0, };
     long long sum = 0;
+    int idx = -1;
  
     cin >> N;
+    int* heights = new int[N] {0};
     for (int i = 0; i < N; i++) {
-        cin >> heights[i];
+        int temp = 0;
+        cin >> temp;
+        while (idx >= 0 && temp >= heights[idx]) {
+            sum += idx;
+            idx--;
+        }
+        heights[++idx] = temp;
+    }
+    while (idx > 0) {
+        sum += idx;
+        idx--;
     }
  
-    int idx = 0;
-    for (int i = 0; i < N; i++) {
-        if (top < 0) stack[++top] = i;
-        else {
-            while (top > -1 && heights[stack[top]] <= heights[i]) {
-                sum += i - stack[top] - 1;
-                stack[top] = 0;
-                top--;
-            }
-            stack[++top] = i;
-        }
-    }
-    /*while(heights[idx]) {
-        if (top < 0) stack[++top] = idx;
-        else {
-            while (top > -1 && heights[stack[top]] <= heights[idx]) {
-                sum += idx - stack[top] - 1;
-                stack[top] = 0;
-                top--;
-            }
-            stack[++top] = idx;
-        }
-        idx++;
-    }*/
-    while (top > -1) {
-        sum += N - stack[top--] - 1;
-    }
     cout << sum << endl;
+    if(heights != nullptr) delete[] heights;
 }
